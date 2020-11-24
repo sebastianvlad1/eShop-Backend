fd78cf3651e48f50cd8db266588785c8b680c447
sebastianvlad1 <sebastianbranzas@gmail.com>
2020-11-24 11:16:15 +0200

    First clean back-end setup

---
+++ Controllers/ProdusController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace eShop_backend.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProdusController : ControllerBase
+    {
+
+        public ProdusController()
+        {
+        }
+
+        [HttpGet("test")]
+        public String Get()
+        {
+            return "Merge";
+        }
+    }
+}
